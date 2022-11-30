8e44ab9593d441ab1355e48144a7496da6e28b3f
Archi903 <artpele@yandex.ru>
2022-11-30 15:38:35 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,18 @@ public class Main {
     public static void main(String[] args) {
         task1();
     }
-    public static void task1 () {
+
+    public static void task1() {
         var dog = 8;
         var cat = 3.6;
         var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
