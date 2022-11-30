b7705ffa75984d691a61d7464d604ee3436cfa61
Archi903 <artpele@yandex.ru>
2022-11-30 15:39:26 +0300

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     }
 
     public static void task1() {
-        var dog = 8;
+        var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
         System.out.println(dog);
@@ -14,6 +14,13 @@ public class Main {
         dog = dog + 4;
         cat = cat + 4;
         paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
