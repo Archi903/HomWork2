7477cc9ad2b42241da4eab021c121ec021fa674e
Archi903 <artpele@yandex.ru>
2022-11-30 15:44:53 +0300

    Домашнее задание 2 "Часть 2"

--- src/Main.java
+++ src/Main.java
@@ -34,6 +34,14 @@ public class Main {
         System.out.println(friend);
         friend = friend / 7;
         System.out.println(friend);
-
+        System.out.println("Часть 2");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
     }
 }
